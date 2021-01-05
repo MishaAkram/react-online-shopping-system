ae098be3ac0024e3986b67c43d7518ca7434f57b
Iqra-Irfan <74137053+Iqra-Irfan@users.noreply.github.com>
2021-01-05 10:15:18 +0500

    added shared folder

---
+++ src/shared/Validity.js
@@ -0,0 +1,25 @@
+export const checkValidity = (value, rules) => {
+    let isValid = true;
+  
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid; // trim() -> pominięcie spacji
+    };
+  
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    };
+  
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    };
+  
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid
+    }
+  
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid
+      return isValid;
+    }
+  
