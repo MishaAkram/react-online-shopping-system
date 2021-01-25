c6658f928466e11f45f34be9b1938926ff0c9650
Iqra-Irfan <74137053+Iqra-Irfan@users.noreply.github.com>
2021-01-26 02:24:03 +0500

    updated product list

--- src/components/Navigation/NavigationItems/SideNavigation.js
+++ src/components/Navigation/NavigationItems/SideNavigation.js
@@ -7,72 +7,67 @@ import '../Navigation.scss';
 import NavigationItem from './NavigationItem';
 
 const femaleCategories = [
-  {
-    category: 'female',
-    content: 'Women',
-    linkType: 'main'
-  },
-  {
-    category: 'women-coats',
-    content: 'Coats'
-  },
-  {
-    category: 'women-jackets',
-    content: 'Jackets',
-  },
-  {
-    category: 'women-suits',
-    content: 'Suits',
-  },
-  {
-    category: 'women-shirts',
-    content: 'Shirts',
-  },
-  {
-    category: 'women-t-shirts',
-    content: 'T-shirts',
-  },
-  {
-    category: 'women-shoes',
-    content: 'Shoes',
-  },
-  {
-    category: 'women-hats',
-    content: 'Hats',
-  },
-  {
-    category: 'male',
-    content: 'Men',
-    linkType: 'main'
-  },
-  {
-    category: 'men-coats',
-    content: 'Coats',
-  },
-  {
-    category: 'men-jackets',
-    content: 'Jackets',
-  },
-  {
-    category: 'men-suits',
-    content: 'Suits',
-  },
-  {
-    category: 'men-shirts',
-    content: 'Shirts',
-  },
-  {
-    category: 'men-t-shirts',
-    content: 'T-shirts',
-  },
-  {
-    category: 'men-shoes',
-    content: 'Shoes',
-  },
-  {
-    category: 'men-hats',
-    content: 'Hats',
-  },
+ // {
+   // category: 'women-coats',
+   // content: 'Coats'
+ // },
+  //{
+   // category: 'women-jackets',
+    //content: 'Jackets',
+ // },
+  // {
+  //   category: 'women-suits',
+  //   content: 'Suits',
+  // },
+  // {
+  //   category: 'women-shirts',
+  //   content: 'Shirts',
+  // },
+  // {
+  //   category: 'women-t-shirts',
+  //   content: 'T-shirts',
+  // },
+  // {
+  //   category: 'women-shoes',
+  //   content: 'Shoes',
+  // },
+  // {
+  //   category: 'women-hats',
+  //   content: 'Hats',
+  // },
+  // {
+  //   category: 'male',
+  //   content: 'Men',
+  //   linkType: 'main'
+  // },
+  // {
+  //   category: 'men-coats',
+  //   content: 'Coats',
+  // },
+  // {
+  //   category: 'men-jackets',
+  //   content: 'Jackets',
+  // },
+  // {
+  //   category: 'men-suits',
+  //   content: 'Suits',
+  // },
+  // {
+  //   category: 'men-shirts',
+  //   content: 'Shirts',
+  // },
+  // {
+  //   category: 'men-t-shirts',
+  //   content: 'T-shirts',
+  // },
+  // {
+  //   category: 'men-shoes',
+  //   content: 'Shoes',
+  // },
+  // {
+  //   category: 'men-hats',
+  //   content: 'Hats',
+  // },
 ];
 
 const sideNavigation = ({ filterProducts, children }) => (
